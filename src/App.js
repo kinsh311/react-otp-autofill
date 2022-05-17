48bb11d742bc99fbec3da1cb2cd4d64bb50205d3
Kinshuk <kinshuksharma311@gmail.com>
2022-05-18 02:48:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const controller = new AbortController();
+  useEffect(() => {
+    console.log("use effect");
+    const runThis = async () => {
+      try {
+        if (navigator.credentials) {
+          try {
+            console.log("stage: 1")
+            await navigator.credentials
+              .get({ signal: controller.signal, otp: { transport: ["sms"] } })
+              .then((content) => {
+                console.log("stage: 2")
+                if (content) {
+                  alert(content)
+                  console.log("content", content)
+                }
+              })
+              .catch((e) => {
+                console.log("error in then content", e);
+              });
+          } catch (e) {
+            console.log("error in then content", e);
+            return
+          }
+        }
+      } catch (err) {
+        
+        console.log("error in then content", err)
+      }
+    }
+    runThis()
+
+
+
+  })
+
+  return <div className="App">
+    <h2>Your Otp is:  </h2>
+    < form action="ok.html" method="POST">
+      < input type="text" className="form-control" id="OTP" placeholder="OTP" autoComplete="one-time-code"/>
+      < button className="btn btn-primary btn-style" id="validateOTP">Verify</button>
+    </form>
+  </div>
 }
 
 export default App;
+
